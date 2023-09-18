f1dbf654d74ea4b75e748fc137ee5b4f42d0334f
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-09-18 14:51:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	//alert("hi");
 	var firstname=document.getElementById("fname").value;
 	var lastname=document.getElementById("lname").value;
 
 	alert("First name: " + firstname + "\nLast name: " + lastname);
 }
+//in html there is prewrritten code yes sir ok sir ,no sir ok sir
