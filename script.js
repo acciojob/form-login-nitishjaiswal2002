9c3da27ecd63d2b4b82fb4752ed438b1e0e575e1
nitishjaiswal2002 <141999440+nitishjaiswal2002@users.noreply.github.com>
2023-09-18 13:46:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	var firstname=document.getElementById("Fname").value;
+	var firstname=document.getElementById("fname").value;
 	var lastname=document.getElementById("lname").value;
 
 	alert("First name: " + firstname + "\nLast name: " + lastname);
